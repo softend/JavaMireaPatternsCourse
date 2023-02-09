1a20acc393ec6537dce7a7868620dc9502f664bc
softend <ilya.zenchenko@icloud.com>
2023-02-09 19:18:48 +0300

    add

--- src/Ptactice1/Main.java
+++ src/Ptactice1/Main.java
@@ -5,11 +5,14 @@ import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
+        //Создаем компаратор и реализуем его функцию compare с помощью лямбда-выражения, она сравнивает размер двух списков
+        //Тип данных компаратора - Список с любыми элементами
         Comparator<ArrayList<?>> comparator = (a, b) -> Integer.compare(a.size(), b.size());
 
         ArrayList<String> list1 = new ArrayList<>();
         ArrayList<String> list2 = new ArrayList<>();
         list1.add("asd");
+        //Создаем 2 списка и тестируем вывод
         System.out.println(comparator.compare(list1,list2));
     }
 }
